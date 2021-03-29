5cd478f29acf2d59ecc69aef969658eb6e25e53d
Kauana Maria Agostini <kauanaagostini@gmail.com>
2021-03-29 14:58:31 -0300

    criado Get do orders, trazendo os produtos das ordens.

--- server/controller/OrdersController.js
+++ server/controller/OrdersController.js
@@ -1,7 +1,58 @@
-const dataBase = require("../db/models")
+const { restart } = require("nodemon");
+const dataBase = require("../db/models");
 
 
 class OrdersController {
+
+  static async getAllOrders(req, res, next) {
+    try {
+      const allOrders = await dataBase.Orders.findAll({
+        include: [{
+          model: dataBase.Products,
+          as: 'products',
+          required: false,
+          attributes: [
+            'id', 'name', 'type', 'price', 'flavor', 'complement'
+          ],
+          through: {
+            model: dataBase.ProductsOrders,
+            as: 'productsOrders',
+            attributes: ['qtd']
+          }
+        }]
+      })
+      return res.status(200).json({allOrders})
+    } catch(err){
+      return res.status(400).json({ error : err.message })
+    }
+  }
+
+  static async getOrderById(req, res, next){
+    const { id } = req.params
+    try {
+      const byId = await dataBase.Orders.findAll({
+        where: {
+          id: Number(id)
+        },
+        include: [{
+          model: dataBase.Products,
+          as: 'products',
+          required: false,
+          attributes: [
+            'id', 'name', 'type', 'price', 'flavor', 'complement'
+          ],
+          through: {
+            model: dataBase.ProductsOrders,
+            as: 'productsOrders',
+            attributes: ['qtd']
+          }
+        }]
+      })
+      return res.status(200).json(byId)
+    } catch(err) {
+      return res.status(400).json({ error: err.message })
+    }
+  }
 
   static async createOrder(req, res, next) {
     try {
--- server/routes/OrdersRouter.js
+++ server/routes/OrdersRouter.js
@@ -4,8 +4,8 @@ const OrdersController = require('../controller/OrdersController')
 const router = Router()
 
 // aqui vai as requisições
-// router.get("/", UsersController.getAllUsers)
-// router.get("/:id", UsersController.getUserById)
+router.get("/", OrdersController.getAllOrders)
+router.get("/:id", OrdersController.getOrderById)
 router.post("/", OrdersController.createOrder)
 // router.put("/:id", UsersController.updateUser)
 // router.delete("/:id", UsersController.deleteUser)
